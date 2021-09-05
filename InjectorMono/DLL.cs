397662ea7b7571d66de6b37a7de84166f04fc6ba
DeftSolutions-dev <59990384+DeftSolutions-dev@users.noreply.github.com>
2021-09-05 19:26:02 -0400

    Add files via upload

---
+++ InjectorMono/DLL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InjectorMono
+{
+    class DLL
+    {
+        //Файл в Hex(Байты) онлайн - https://goo-gl.su/file-to-hex
+        public static byte[] dll = {
+
+};
+    }
+}
